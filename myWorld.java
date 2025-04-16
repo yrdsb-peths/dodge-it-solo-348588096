1bc33c0d2973f9823c019afbbcdf752c89ccb360
348588096 <veznup@gmail.com>
2025-04-16 14:16:35 -0400

    Create object on screen

--- myWorld.java
+++ myWorld.java
@@ -1,10 +1,15 @@
-import greenfoot.*;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class MyWorld extends World {
-    public MyWorld() {
+
+public class MyWorld extends World
+{
+
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         Banana banana = new Banana();
-        addObject(banana, 100,100);
+        addObject(banana, 300, 200);
         
     }
 }
