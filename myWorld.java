201fc2cac7856d75e3e91f585b98003373b36afb
348588096 <veznup@gmail.com>
2025-04-17 14:00:45 -0400

    Update myWorld.java

--- myWorld.java
+++ myWorld.java
@@ -9,11 +9,14 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         Banana banana = new Banana();
-        addObject(banana, 300, 200);
+        addObject(banana, 600, 200);
         
         
-        Kangaroo kangaroo = new Kangaroo();
-        addObject(kangaroo,0,200);
+        Kangaroo kangarooOne = new Kangaroo();
+        addObject(kangarooOne,0,200);
+        
+        Kangaroo kangarooTwo = new Kangaroo();
+        addObject(kangarooTwo,0,200);
         
         Tent tent = new Tent();
         addObject(tent,500,200);
