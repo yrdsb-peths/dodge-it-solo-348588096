2d747ab8c5a67907095a2db42ea1ca0420c4def9
348588096 <veznup@gmail.com>
2025-04-17 14:09:42 -0400

    Remove Objects and add Moving Ball

--- MyWorld.java
+++ MyWorld.java
@@ -8,21 +8,14 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
-        Banana banana = new Banana();
-        addObject(banana, 600, 200);
-        
+ 
         
         Kangaroo kangarooOne = new Kangaroo();
-        addObject(kangarooOne,0,200);
+        addObject(kangarooOne,300,200);
         
-        Kangaroo kangarooTwo = new Kangaroo();
-        addObject(kangarooTwo,200,200);
+        Ball ball = new Ball();
+        addObject(ball,0,200);
         
-        Tent tent = new Tent();
-        addObject(tent,500,200);
-        
-        Butterfly butterfly = new Butterfly();
-        addObject(butterfly,0,100);
         
         
     }
