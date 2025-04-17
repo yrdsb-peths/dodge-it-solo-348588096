2897c0e05452c31dcb0cf8296317540886b2e4f9
348588096 <veznup@gmail.com>
2025-04-17 14:21:05 -0400

    Change Scope Of Variable

--- Kangaroo.java
+++ Kangaroo.java
@@ -8,13 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Kangaroo extends Actor
 {
-    /**
-     * Act - do whatever the Monkey wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    boolean atTop = true;
     public void act()
     {
-        boolean atTop = true;
         
         if(Greenfoot.mouseClicked(null))
         {
